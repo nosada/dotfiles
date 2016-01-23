0cedacbd907e284a820ee2eb262232305c583eec
NOGISAKA Sadata <ngsksdt@gmail.com>
2016-01-23 18:50:14 +0900

    check and deploy on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
-ln -s ${PWD}/vimrc ${HOME}/.vimrc
-ln -s ${PWD}/vim ${HOME}/.vim
-mkdir -p ${HOME}/.vim/bundle/
-git clone https://github.com/Shougo/neobundle.vim ${HOME}/.vim/bundle/neobundle.vim
+if [ -h ${HOME}/.tmux.conf ]; then
+	echo "${HOME}/.tmux.conf already exists."
+else
+	ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
+fi
+
+if [ -h ${HOME}/.vimrc ]; then
+	echo "${HOME}/.vimrc already exists."
+else
+	ln -s ${PWD}/vimrc ${HOME}/.vimrc
+fi
+
+if [ -h ${HOME}/.vim ]; then
+	echo "${HOME}/.vim/ already exists."
+else
+	ln -s ${PWD}/vim ${HOME}/.vim
+fi
+
+if [ -d ${HOME}/.vim/bundle ]; then
+	echo "${HOME}/.vim/bundle/ already exists."
+else
+	mkdir -p ${HOME}/.vim/bundle/
+	git clone https://github.com/Shougo/neobundle.vim ${HOME}/.vim/bundle/neobundle.vim
+fi
