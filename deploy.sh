512533b38c3b8b2149d41de747dcc51ea7e28426
NOGISAKA Sadata <ngsksdt@gmail.com>
2016-01-23 15:27:48 +0900

    hidden files is now unhidden

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ${PWD}/.tmux.conf ${HOME}/.tmux.conf
-ln -s ${PWD}/.vimrc ${HOME}/.vimrc
-ln -s ${PWD}/.vim ${HOME}/.vim
+ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
+ln -s ${PWD}/vimrc ${HOME}/.vimrc
+ln -s ${PWD}/vim ${HOME}/.vim
 mkdir -p ${HOME}/.vim/bundle/
