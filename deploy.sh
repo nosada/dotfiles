feefe0b5f1885e7553a5225b3db97cfe71b1a895
NOGISAKA Sadata <ngsksdt@gmail.com>
2016-01-23 15:42:57 +0900

    install neobundle automatically by deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
 ln -s ${PWD}/vimrc ${HOME}/.vimrc
 ln -s ${PWD}/vim ${HOME}/.vim
 mkdir -p ${HOME}/.vim/bundle/
+git clone https://github.com/Shougo/neobundle.vim ${HOME}/.vim/bundle/neobundle.vim
