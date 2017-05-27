75fb8769bd00c67d16986cfd0bae316a724162d3
NOGISAKA Sadata <ngsksdt@gmail.com>
2017-05-27 21:23:22 +0900

    add newsbeuter config

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# deploy tmux config
 if [ -h ${HOME}/.tmux.conf ]; then
 	echo "${HOME}/.tmux.conf already exists."
 else
 	ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
 fi
 
+# deploy and setup newsbeuter
+if [ -h ${HOME}/.config/newsbeuter ]; then
+	echo "${HOME}/.config/newsbeuter/ already exists."
+else
+	ln -s ${PWD}/newsbeuter ${HOME}/.config/newsbeuter
+fi
+if [ -e ${HOME}/.local/share/newsbeuter ];then
+	echo "${HOME}/.local/share/newsbeuter/ already exists."
+else
+	mkdir -p ${HOME}/.local/share/newsbeuter
+fi
+
+# deploy vim (or neovim, if used) config
 if [ -e /usr/bin/vim ]; then
 	if [ -h ${HOME}/.vimrc ]; then
 		echo "${HOME}/.vimrc already exists."
