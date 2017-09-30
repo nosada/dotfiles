2d36fd241e48a2b499c5da0b343f068ff887a45d
NOGISAKA Sadata <ngsksdt@gmail.com>
2017-09-30 22:52:50 +0900

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -7,22 +7,22 @@ function check_vim_existence() { which "vim" &> /dev/null; }
 function check_neovim_existence() { which "nvim" &> /dev/null; }
 
 # remove symlink of tmux config
-[ -e "${HOME}/.tmux.conf" ] && rm -f ${HOME}/.tmux.conf
+rm -f ${HOME}/.tmux.conf
 
 # remove symlink and directory for newsbeuter
-[ -e "${USER_CONF_DIR}/newsbeuter" ] && rm -f ${USER_CONF_DIR}/newsbeuter
-[ -e "${USER_LOCAL_DIR}/share/newsbeuter" ] && rm -rf ${USER_LOCAL_DIR}/share/newsbeuter
+rm -f ${USER_CONF_DIR}/newsbeuter
+rm -rf ${USER_LOCAL_DIR}/share/newsbeuter
 
 # remove vim symlinks
-check_vim_existence && [ -e "${HOME}/.vimrc" ] && rm -f ${HOME}/.vimrc
-check_vim_existence && [ -e "${HOME}/.vim" ] && rm -f ${HOME}/.vim
+check_vim_existence && rm -f ${HOME}/.vimrc
+check_vim_existence && rm -f ${HOME}/.vim
 
 # remove neovim symlink
-check_neovim_existence && [ -h "${HOME}/.config/nvim" ] && rm -f ${HOME}/.config/nvim
+check_neovim_existence && rm -f ${HOME}/.config/nvim
 
 # remove fish symlinks
-[ -e "${FISH_DIR}/config.fish" ] && rm -f ${FISH_DIR}/config.fish 
-[ -e "${FISH_DIR}/fishfile" ] && rm -f ${FISH_DIR}/fishfile
+rm -f ${FISH_DIR}/config.fish
+rm -f ${FISH_DIR}/fishfile
 
 # check existense of maybe-already-deployed configs
 [ -h "${HOME}/.tmux.conf" ] && exit 1
