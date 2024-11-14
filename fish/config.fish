e5a083c9c8fb5780e5b727d65f5053b4427fb8d5
NOGISAKA Sadata <ngsksdt@gmail.com>
2024-11-15 02:21:29 +0900

    Load Homebrew shellenv if available

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,10 @@ if which -a nvim > /dev/null
   set -x EDITOR nvim
 end
 
+if test -e /opt/homebrew/bin/brew
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+
 if status is-interactive
   fish_vi_key_bindings
 end
